ad30bcdf4b7efa2756b4348605fa6c39e2d9e4d2
vaisakh-pc <vaisakh-pc@VAISAKH-PC>
2019-02-24 10:27:08 +0530

    Add project files.

---
+++ Pepper/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Pepper.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Pepper/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pepper.WebApiApplication" Language="C#" %>
